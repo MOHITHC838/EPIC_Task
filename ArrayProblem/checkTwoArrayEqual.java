d204b05202b02903a1dc3ff003f38834bec9de28
MOHITHC838 <mrcmsmohith04@gmail.com>
2026-07-21 19:52:45 +0530

    Operator completed

--- ArrayProblem/checkTwoArrayEqual.java
+++ ArrayProblem/checkTwoArrayEqual.java
@@ -3,22 +3,26 @@ package ArrayProblem;
 public class checkTwoArrayEqual {
     public static void main(String[] args) {
         int[] array_1 = {1,2,3,4};
-        int[] array_2 = {1,1,3,4};
+        int[] array_2 = {1,2,3,4};
 
         // if (array_1.length == array_2.length) {
         //     System.out.println("equal");     
         // }else{
         //     System.out.println("Not equal");
         // }
+        
+        boolean op = true;
         for(int i=0;i<=array_1.length-1;i++){
-            if (array_1[i] == array_2 [i]) {
-            boolean op = true;
+            if (array_1[i] != array_2 [i]) {
+             op = false;
             }
     }
-        System.out.println("equal");
-
-      
-        
+    if(op){
+        System.out.println("Equal");
+    }  
+    else{
+        System.out.println("not equal");
+    }  
 
     }
 }
