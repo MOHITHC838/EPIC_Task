58754560d67927999d8c6214922c39950d0d9f20
MOHITHC838 <mrcmsmohith04@gmail.com>
2026-07-24 23:15:54 +0530

    x pattern complete

--- Looping/multiplication.java
+++ Looping/multiplication.java
@@ -3,7 +3,6 @@ package Looping;
 public class multiplication {
     public static void main(String[] args) {
         int n = 5;
-        int multi =0;
 
         for(int i=1;i<=10;i++){
           System.out.println(i + "X" + n + "=" + i*n);
